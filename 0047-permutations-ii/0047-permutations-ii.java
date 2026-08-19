795fd28271f1e0d00d3f5b3ff959347fe834c736
OMKAR.R.SATHE <omkarsathe3103@gmail.com>
2026-08-19 22:26:06 +0530

    Time: 3 ms (92.09%), Space: 46.4 MB (88.98%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+class Solution {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        Arrays.sort(nums);
+
+        boolean[] used = new boolean[nums.length];
+
+        backtrack(nums, used, new ArrayList<>(), result);
+
+        return result;
+    }
+
+    private void backtrack(int[] nums, boolean[] used,
+                            List<Integer> current,
+                            List<List<Integer>> result) {
+
+        // A complete permutation is created
+        if (current.size() == nums.length) {
+            result.add(new ArrayList<>(current));
+            return;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+
+            // Number is already used
+            if (used[i]) {
+                continue;
+            }
+
+            // Skip duplicate choices
+            if (i > 0 && nums[i] == nums[i - 1] && !used[i - 1]) {
+                continue;
+            }
+
+            // Choose
+            used[i] = true;
+            current.add(nums[i]);
+
+            // Explore
+            backtrack(nums, used, current, result);
+
+            // Undo choice
+            current.remove(current.size() - 1);
+            used[i] = false;
+        }
+    }
+}
